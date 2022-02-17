5222104cd5a2767ec6332141cd5d09cf25d337b1
Kayrat <72617688+makhmudov-kay@users.noreply.github.com>
2022-02-17 17:33:45 +0500

    mark-up nitification page and business card modal component

--- projects/client/src/app/modules/agro-id/pages/cabinet/cabinet.routing.ts
+++ projects/client/src/app/modules/agro-id/pages/cabinet/cabinet.routing.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CabinetComponent } from './cabinet.component';
 import { BiznesCardsComponent } from './pages/biznes-cards/biznes-cards.component';
 import { LegalPersonComponent } from './pages/legal-person/legal-person.component';
+import { NotificationComponent } from './pages/notification/notification.component';
 import { PersonalComponent } from './pages/personal/personal.component';
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
       { path: 'personal', component: PersonalComponent },
       { path: 'legalperson', component: LegalPersonComponent },
       { path: 'biznescards', component: BiznesCardsComponent },
+      { path: 'notification', component: NotificationComponent },
     ],
   },
 ];
--- projects/client/src/app/modules/agro-id/pages/cabinet/components/business-card-modal/business-card-modal.component.ts
+++ projects/client/src/app/modules/agro-id/pages/cabinet/components/business-card-modal/business-card-modal.component.ts
@@ -1,50 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
+export interface BusinessCardConfig {
+  isVisibleEditModal: boolean;
+  title: string;
+  data?: UserCard;
+}
+
+export interface UserCard {
+  firstName: string;
+  lastName: string;
+  phone: string[];
+  avatar: string;
+  mail: string;
+}
+
+
 @Component({
-  selector: 'app-business-card-modal',
+  selector: 'business-card-modal',
   templateUrl: './business-card-modal.component.html',
   styleUrls: ['./business-card-modal.component.less'],
 })
 export class BusinessCardModalComponent implements OnInit {
-  visibleEditModal = false;
+  @Input() config!: BusinessCardConfig;
+
+  @Output() invisibleModal = new EventEmitter<boolean>();
   validateForm!: FormGroup;
   constructor(private msg: NzMessageService, private fb: FormBuilder) {}
 
   ngOnInit() {
+    console.log(this.config);
+    
     this.validateForm = this.fb.group({
-      firstName: [null, [Validators.required]],
-      lastName: [null, [Validators.required]],
-      phone: [null, [Validators.required]],
-      additionalPhone: [null],
-      mail: [null],
+      firstName: [this.config.data?.firstName, [Validators.required]],
+      lastName: [this.config.data?.lastName, [Validators.required]],
+      phone: [this.config.data?.phone[0], [Validators.required]],
+      additionalPhone: [this.config.data?.phone?.[1]],
+      mail: [this.config.data?.mail],
     });
+  }
+
+  toggleModal(event: boolean): void {
+    this.invisibleModal.emit(event)
   }
 
   handleOk(): void {
     console.log('Button ok clicked!');
-    this.visibleEditModal = false;
+    this.config.isVisibleEditModal = false;
   }
 
   handleCancel(): void {
     console.log('Button cancel clicked!');
-    this.visibleEditModal = false;
+    this.config.isVisibleEditModal = false;
   }
 
   showEditModal(): void {
-    this.visibleEditModal = true;
+    this.config.isVisibleEditModal = true;
   }
 
   editModalOk(): void {
     console.log('Button ok clicked!');
-    this.visibleEditModal = false;
+    this.config.isVisibleEditModal = false;
   }
 
   editModalCancel(): void {
     console.log('Button cancel clicked!');
-    this.visibleEditModal = false;
+    this.config.isVisibleEditModal = false;
   }
 
   handleChange(info: NzUploadChangeParam): void {
