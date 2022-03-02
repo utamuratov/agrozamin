d65f41fe4f25b19405f832d7a65287c8f59bb55f
utamuratov <43562558+utamuratov@users.noreply.github.com>
2022-03-02 16:44:00 +0500

    removed unused codes

--- projects/admin/src/app/pages/translate/interface/interface.component.ts
+++ projects/admin/src/app/pages/translate/interface/interface.component.ts
@@ -25,7 +25,10 @@ export class InterfaceComponent implements OnInit {
     }
   }
 
-  size: NzButtonSize = 'default';
+  /**
+   *
+   */
+  isVisibleModal = false;
 
   /**
    *
@@ -37,57 +40,9 @@ export class InterfaceComponent implements OnInit {
    */
   filteredData: ItemData[] = [];
 
-  /**
-   *
-   */
-  searchText = '';
-
-  /**
-   *
-   */
-  searchBy: 'id' | 'name' | 'login' = 'name';
-  /**
-   *
-   */
-
-  filteredOptions: string[] = [];
-  /**
-   *
-   */
-
-  options = ['Ru', 'Eng', 'Key'];
-  /**
-   *
-   */
-
-  switchValue = false;
-  switchValue1 = false;
-
-  isVisible = false;
-  isOkLoading = false;
-
-  constructor(private $translate: TranslateApiService) {
-    this.filteredOptions = this.options;
-  }
+  constructor(private $translate: TranslateApiService) {}
 
   ngOnInit(): void {
-    const data = [];
-    for (let i = 0; i < 100; i++) {
-      data.push({
-        login: '',
-        id: i,
-        name: `John ${i}`,
-        age: 32,
-        address: `London, Park Lane no. ${i}`,
-        description:
-          'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-      });
-    }
-    this.data = data;
-    this.filteredData = data;
-
-    this.search();
-
     this.getTranslations();
   }
 
@@ -99,56 +54,11 @@ export class InterfaceComponent implements OnInit {
   }
 
   // Show Modal
-  showModal(): void {
-    this.isVisible = true;
+  openModal(): void {
+    this.isVisibleModal = true;
   }
 
-  handleOk(): void {
-    this.isOkLoading = true;
-    setTimeout(() => {
-      this.isVisible = false;
-      this.isOkLoading = false;
-    }, 3000);
-  }
-
-  handleCancel(): void {
-    this.isVisible = false;
-  }
-
-  // INput Handler
-
-  changeInput(event: any) {
-    this.filteredOptions = this.options.filter(
-      (option) => option.toLowerCase().indexOf(event.toLowerCase()) !== -1
-    );
-    if (event === null || event === '') {
-      this.filteredData = this.data;
-    }
-  }
-
-  search() {
-    // console.log(Name)
-    switch (this.searchBy) {
-      case 'name':
-        this.filteredData = this.data.filter((data) =>
-          data.name.includes(this.searchText)
-        );
-        break;
-
-      case 'id':
-        this.filteredData = this.data.filter(
-          (data) => data.id === +this.searchText
-        );
-        break;
-      case 'login':
-        this.filteredData = this.data.filter((data) =>
-          data.login.includes(this.searchText)
-        );
-        break;
-
-      default:
-        break;
-    }
-    console.log(this.searchBy);
+  closeModal(): void {
+    this.isVisibleModal = false;
   }
 }
