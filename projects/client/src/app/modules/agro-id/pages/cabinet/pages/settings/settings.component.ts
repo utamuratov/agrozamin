9c8b11d573f46f7f24a234b6c9606aeea0e41bca
Kayrat <72617688+makhmudov-kay@users.noreply.github.com>
2022-03-04 18:02:44 +0500

    cabinet mark-up

---
+++ projects/client/src/app/modules/agro-id/pages/cabinet/pages/settings/settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'az-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.less']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
