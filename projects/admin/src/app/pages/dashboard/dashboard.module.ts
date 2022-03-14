f25aad3fb33fb02465db9a617dd9a4eb1e0a7834
IsmoiljonAkbarjon <62274325+IsmoiljonAkbarjon@users.noreply.github.com>
2022-03-14 16:41:10 +0500

    translations

--- projects/admin/src/app/pages/dashboard/dashboard.module.ts
+++ projects/admin/src/app/pages/dashboard/dashboard.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -27,6 +28,7 @@ import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
      * NGX-AZ-CORE
      */
     LanguageModule,
+    TranslateModule,
 
     
     NzAvatarModule,
