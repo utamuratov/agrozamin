209ce89c8460ebb5b812c2881c6f70fa46257d98
Kayrat <72617688+makhmudov-kay@users.noreply.github.com>
2022-04-22 16:15:25 +0500

    finish home page with resposive
    закончил верстку сторисов и полностью готова главная страница

---
+++ projects/agro-zamin/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+];
+
+export const AppRoutes = RouterModule.forChild(routes);
