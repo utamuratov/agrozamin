6ab6afbe586be0b45fe96973af29b2b5ecfc775a
IsmoiljonAkbarjon <62274325+IsmoiljonAkbarjon@users.noreply.github.com>
2022-05-05 09:35:48 +0500

    adverticement menu

--- projects/advertisement/src/app/ozimizniki/modules/cabinet/cabinet.module.ts
+++ projects/advertisement/src/app/ozimizniki/modules/cabinet/cabinet.module.ts
@@ -1,3 +1,4 @@
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CabinetComponent } from './cabinet.component';
@@ -6,9 +7,23 @@ import { AdvertisementComponent } from './components/advertisement/advertisement
 import { MessagesComponent } from './components/messages/messages.component';
 import { FavouritesComponent } from './components/favourites/favourites.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
-  imports: [CommonModule, CabinetRoutes, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    CabinetRoutes, 
+    FormsModule, 
+    ReactiveFormsModule,
+    NzGridModule,
+    NzMenuModule,
+    NzButtonModule,
+    NzIconModule,
+    NzToolTipModule
+  ],
   declarations: [
     CabinetComponent,
     AdvertisementComponent,
