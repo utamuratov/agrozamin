445aa26cbbba0d2c5d6b122aac7d51bacb3aa426
IsmoiljonAkbarjon <62274325+IsmoiljonAkbarjon@users.noreply.github.com>
2022-06-17 18:14:00 +0500

    stat-consult-desktop

--- projects/online-consultant/src/app/online-consultant/modules/become-consultant/become-consultant.module.ts
+++ projects/online-consultant/src/app/online-consultant/modules/become-consultant/become-consultant.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NzStepsModule } from 'ng-zorro-antd/steps';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzGridModule } from 'ng-zorro-antd/grid';
@@ -8,17 +9,29 @@ import { CommonModule } from '@angular/common';
 import { BecomeConsultantComponent } from './become-consultant.component';
 import { BecomeConsultantRoutes } from './become-consultant.routing';
 import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 
 @NgModule({
   imports: [
     CommonModule,
     BecomeConsultantRoutes,
+    FormsModule,
+    ReactiveFormsModule,
     NzBreadCrumbModule,
     NzDividerModule,
     NzGridModule,
     NzButtonModule,
     NzModalModule,
-    NzStepsModule
+    NzStepsModule,
+    NzSelectModule,
+    NzInputModule,
+    NzFormModule,
+    NzCheckboxModule,
+    NzDrawerModule
   ],
   declarations: [BecomeConsultantComponent]
 })
