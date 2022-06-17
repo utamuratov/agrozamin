982b68b9e8ce18af13ccd2305355a4ba50d41417
Kayrat <72617688+makhmudov-kay@users.noreply.github.com>
2022-06-17 17:45:48 +0500

    find-consultant mark-up finish

--- projects/online-consultant/src/app/online-consultant/modules/consultant-page/consultant-page.component.ts
+++ projects/online-consultant/src/app/online-consultant/modules/consultant-page/consultant-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDistance } from 'date-fns';
 
 @Component({
   selector: 'az-consultant-page',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultant-page.component.less']
 })
 export class ConsultantPageComponent implements OnInit {
+  likes = 0;
+  time = formatDistance(new Date(), new Date());
+
+  data = {
+    author: 'Мингазова Нина',
+    avatar: './assets/images/comment2.jpg',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources' +
+      '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    children: [
+      {
+        author: 'Артур Уралович ',
+        avatar: './assets/images/comment3.jpg',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources' +
+          '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        children: [
+          {
+            author: 'Тимур Мун',
+            avatar: './assets/images/comment4.jpg',
+            content:
+              'We supply a series of design principles, practical patterns and high quality design resources' +
+              '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.'
+          },
+        ]
+      }
+    ]
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  like(): void {
+    this.likes = 1;
+  }
 }
